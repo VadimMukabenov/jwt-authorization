c1eb1a7559b233c0146749218975914a4fdc5a32
Vadim Mukabenov <vadim.mukabenov98@gmail.com>
2022-07-13 11:55:44 +0300

    add unit tests

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    setupFiles: ["<rootDir>/node_modules/jest-extended/types/index.d.ts"],
     setupFilesAfterEnv: ["<rootDir>/node_modules/jest-extended/all"]
 };
